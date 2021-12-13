60894136eb64b5c9f7f7ec4b4c39ca8ab521ec47
Fatichti <fabienplouvier5@gmail.com>
2021-12-13 16:28:41 +0100

    repo Maxine

---
+++ catkin_ws/build/beginner_tutorials/CMakeFiles/beginner_tutorials_generate_messages_eus.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/maxine.gravier/catkin_ws/devel/share/roseus/ros/beginner_tutorials/manifest.l"
+  "/home/maxine.gravier/catkin_ws/devel/share/roseus/ros/beginner_tutorials/msg/Num.l"
+  "/home/maxine.gravier/catkin_ws/devel/share/roseus/ros/beginner_tutorials/srv/AddTwoInts.l"
+  "CMakeFiles/beginner_tutorials_generate_messages_eus"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/beginner_tutorials_generate_messages_eus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
