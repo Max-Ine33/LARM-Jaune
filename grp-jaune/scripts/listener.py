add54e798290b8d5c85774099ac6c9a098c51652
Ubuntu <Linux-Fab-Max@Linux-LARM.xuvharqrg5qepmd3nbyw5tixne.parx.internal.cloudapp.net>
2021-12-16 00:51:25 +0000

    Dernière MAJ de la journée

--- grp-jaune/scripts/listener.py
+++ grp-jaune/scripts/listener.py
@@ -1,18 +1,48 @@
 #!/usr/bin/python3
 import rospy
-from std_msgs.msg import String
+from sensor_msgs.msg import LaserScan
+import sensor_msgs.msg
+from geometry_msgs.msg import Twist
 
-def callback(data):
+def callback(msg):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    
+    print("##############################################")
+    # values at 0 degree
+    print("s2 [0]")
+    print(msg.ranges[0])
+    # values at 90 degree
+    print("s3 [90]")
+    print(msg.ranges[360])
+    # values at 180 degree
+    print("s4 [180]")
+    print(msg.ranges[719])
+    
+    
+    
+    
+    #If the distance to an obstacle in front of the robot is bigger than 1 meter, the robot will move forward
+    if msg.ranges[0] > 1:
+        move.linear.x = 0.5
+        move.angular.z = 0.0
+    #If the distance to an obstacle in front of the robot is smaller than 1 meter, the robot will stop
+    else:
+        move.linear.x = 0.0
+        move.angular.z = 0.0
+
+    pub.publish(move)
     
 def listener():
     rospy.init_node('listener', anonymous=True)
-
-    rospy.Subscriber("base_scan", String, callback)
+    rospy.Subscriber('/scan', LaserScan, callback)
+    rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+    rospy.Rate(10) # 10hzrospy.init_node('listener', anonymous=True)
+    move = Twist()
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
 
 if __name__ == '__main__':
+    print("start listener.py")
     listener()
--- grp-jaune/scripts/move.py
+++ grp-jaune/scripts/move.py
@@ -2,6 +2,25 @@
 import math, rospy
 from geometry_msgs.msg import Twist
 
+rospy.init_node('node_move', anonymous=True)
+pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+
+def move_command(data):
+    move = Twist()
+    #rate = rospy.Rate(1)
+    move.linear.x = 1
+    #move.angular.z = 1
+    pub.publish(move)
+    
+# call the move_command at a regular frequency:
+rospy.Timer( rospy.Duration(0.1), move_command, oneshot=False )
+
+# spin() enter the program in a infinite loop
+print("Start move.py")
+rospy.spin()
+        
+
+"""
 # Initialize ROS::node
 rospy.init_node('move', anonymous=True)
 
@@ -22,4 +41,4 @@ rospy.Timer( rospy.Duration(0.1), move_command, oneshot=False )
 
 # spin() enter the program in a infinite loop
 print("Start move.py")
-rospy.spin()
+rospy.spin()"""
