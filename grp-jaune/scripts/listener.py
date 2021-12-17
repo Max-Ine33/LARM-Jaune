6975d37b08147daba45a9f3da0e9b9d487ee103e
Ubuntu <Linux-Fab-Max@Linux-LARM.xuvharqrg5qepmd3nbyw5tixne.parx.internal.cloudapp.net>
2021-12-17 00:40:15 +0000

    MAJ du soir 16_12

--- grp-jaune/scripts/listener.py
+++ grp-jaune/scripts/listener.py
@@ -5,44 +5,43 @@ import sensor_msgs.msg
 from geometry_msgs.msg import Twist
 
 def callback(msg):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-    
+    #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
     print("##############################################")
-    # values at 0 degree
-    print("s2 [0]")
-    print(msg.ranges[0])
-    # values at 90 degree
-    print("s3 [90]")
-    print(msg.ranges[360])
-    # values at 180 degree
-    print("s4 [180]")
-    print(msg.ranges[719])
+    print("Value at 0 degree:", msg.ranges[0])
+    print("values at 90 degree", msg.ranges[360])
+    print("values at 180 degree", msg.ranges[719])
     
-    
-    
-    
+
     #If the distance to an obstacle in front of the robot is bigger than 1 meter, the robot will move forward
-    if msg.ranges[0] > 1:
-        move.linear.x = 0.5
+    if msg.ranges[360] < 1.0:
+        print("obstacle devant")
+    if msg.ranges[719] < 1.0:
+        print("obstacle côté gauche")
+    if msg.ranges[0] < 1.0:
+        print("obstacle côté droit")
+    if msg.ranges[360] > 1.0:
+        print("je vais tout droit")
+        move.linear.x = 1.0
         move.angular.z = 0.0
     #If the distance to an obstacle in front of the robot is smaller than 1 meter, the robot will stop
+    elif msg.ranges[719] > 1.0 and msg.ranges[360] < 1.0:
+        print("je vais à gauche")
+        move.linear.x = -1.0
+        #move.linear.z = 1.0
     else:
         move.linear.x = 0.0
         move.angular.z = 0.0
 
     pub.publish(move)
     
-def listener():
-    rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber('/scan', LaserScan, callback)
-    rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-    rospy.Rate(10) # 10hzrospy.init_node('listener', anonymous=True)
-    move = Twist()
-
+#def listener():
+rospy.init_node('listener', anonymous=True)
+sub = rospy.Subscriber('/base_scan', LaserScan, callback)
+pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+rospy.Rate(10) # 10hzrospy.init_node('listener', anonymous=True)
+move = Twist()
     # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+print("start listener.py")
+rospy.spin()
 
-
-if __name__ == '__main__':
-    print("start listener.py")
-    listener()
+print("start listener.py")
--- grp-jaune/scripts/move.py
+++ grp-jaune/scripts/move.py
@@ -8,8 +8,6 @@ pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 def move_command(data):
     move = Twist()
     #rate = rospy.Rate(1)
-    move.linear.x = 1
-    #move.angular.z = 1
     pub.publish(move)
     
 # call the move_command at a regular frequency:
@@ -18,27 +16,4 @@ rospy.Timer( rospy.Duration(0.1), move_command, oneshot=False )
 # spin() enter the program in a infinite loop
 print("Start move.py")
 rospy.spin()
-        
-
-"""
-# Initialize ROS::node
-rospy.init_node('move', anonymous=True)
-
-commandPublisher = rospy.Publisher(
-    '/cmd_vel',
-    Twist, queue_size=10
-)
-
-# Publish velocity commandes:
-def move_command(data):
-    # Compute cmd_vel here and publish... (do not forget to reduce timer duration)
-    cmd= Twist()
-    cmd.linear.x= 1
-    commandPublisher.publish(cmd)
-
-# call the move_command at a regular frequency:
-rospy.Timer( rospy.Duration(0.1), move_command, oneshot=False )
-
-# spin() enter the program in a infinite loop
-print("Start move.py")
-rospy.spin()"""
+       
