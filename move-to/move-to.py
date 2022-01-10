f213525a8f7e895a0b90b86768e7513a30761cbb
Fatichti <fabienplouvier5@gmail.com>
2022-01-10 11:04:33 +0100

    MAJ move to

---
+++ move-to/move-to.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import math, rospy
+import tf
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from std_msgs.msg import String
+
+
+# Initialize ROS::node
+rospy.init_node('move', anonymous=True)
+
+
+commandPublisher = rospy.Publisher(
+    '/cmd_vel',
+    Twist, queue_size=10
+)
+
+
+def callback(data):
+	rospy.loginfo("I heard %s",data.pose.pose)
+	
+	
+# Publish velocity commandes:
+def move_command(data):
+    # Compute cmd_vel here and publish... (do not forget to reduce timer duration)
+    cmd= Twist()
+    cmd.linear.x= 1
+    commandPublisher.publish(cmd)
+
+# call the move_command at a regular frequency:
+#rospy.Timer( rospy.Duration(0.1), move_command, oneshot=False )
+
+# spin() enter the program in a infinite loop
+print("Start move.py")
+rospy.Subscriber('goal',Odometry,callback)
+rospy.spin()
